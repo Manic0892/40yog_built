537248ae8004f14fd2167d16d0f8099323eb9ab1
Manic <basbalmanic@gmail.com>
2014-05-19 02:54:41 -0700

    Scrolling.

--- site/lib/main.js
+++ site/lib/main.js
@@ -2,4 +2,5 @@ $(window).load(function() {
 	var debug = getParameterByName('debug');
 	if (debug == 'true' || debug == '1')
 		$('.ig_debug').show('slow');
+	$('html, body').animate({scrollTop:$('#game').position().top}, 'slow');
 });
